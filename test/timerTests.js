fe008ab9580977efc71df5ff8707cf6153efc9ef
Golo Roden <webmaster@goloroden.de>
2013-01-04 15:50:35 +0100

    Interims checkin.

--- test/timerTests.js
+++ test/timerTests.js
@@ -5,5 +5,29 @@ var assert = require('node-assertthat');
 var timer = require('../lib/timer');
 
 suite('timer', function () {
-  
+  suite('create', function () {
+    test('returns a timer that emits periodically.', function (done) {
+      var t = timer.create(100),
+          counter = 0;
+      t.on('elapsed', function () {
+        counter++;
+      });
+      setTimeout(function () {
+        assert.that(counter, is.equalTo(5));
+        done();
+      }, 550);
+    });
+
+    test('returns a timer that emits immediately if specified.', function (done) {
+      var t = timer.create(100, { immediate: true }),
+          counter = 0;
+      t.on('elapsed', function () {
+        counter++;
+      });
+      setTimeout(function () {
+        assert.that(counter, is.equalTo(6));
+        done();
+      }, 550);
+    });
+  });
 });
